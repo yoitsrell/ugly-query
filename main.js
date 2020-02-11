30b9b4840d214c19ea0ba6b78304ac2c9735b40c
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-02-11 03:07:32 -0500

    Initial

---
+++ main.js
@@ -0,0 +1,65 @@
+const background = document.querySelector('body')
+background.style.backgroundColor = 'lightblue'
+background.style.fontSize = '5em'
+// background.style.background = "url('http://giphy.com/gifs/creepy-static-EO9j0o6pHdYmk')"
+
+const ty = document.querySelector('#ty')
+ty.innerText = "The snazzberries taste like snazzberries"
+
+const title1 = document.querySelector('h1')
+title1.style.fontSize = "3em"
+title1.style.transform = 'skewX(-30deg)'
+title1.style.transform = 'skewY(80deg)'
+title1.style.color = 'YellowGreen'
+title1.innerHTML = "My Mom Says I'm Pretty"
+
+const cite = document.querySelector('cite');
+cite.innerText = "We're all mad here"
+cite.style.fontSize = '500px'
+cite.style.color = 'white'
+cite.style.transform = 'skewXY(-20deg,20deg)'
+
+const head3= document.querySelector('h3')
+head3.innerText = "WORK IN PROGRESS"
+head3.style.backgroundSize = '2em'
+head3.style.border = "thick solid #128540"
+head3.style.borderBottomColor = 'black'
+head3.style.transform = 'skewY(20deg)'
+head3.style.transform = 'rotate(-38deg)'
+head3.style.opacity = '75%'
+
+const div2 = document.querySelector('#two')
+div2.style.border = 'black'
+div2.style.border = 'thick dotted sienna 2em'
+div2.style.transform = 'skewX(72deg)'
+
+const div3 = document.querySelector('#three')
+div3.style.borderBottomColor = 'black'
+div3.style.border = 'thick dotted sienna 2em'
+div3.style.transform = 'skewX(56deg)'
+div3.style.transform = 'skewY(-80deg)'
+
+const div4 = document.querySelector('#four')
+div4.style.border = 'thick dotted black 2em'
+div4.style.transform = 'skewY(80deg)'
+
+const text2 = document.querySelector('text2')
+text2.style.transform = 'rotate(275deg)'
+
+
+const line1= document.querySelector('li1')
+li1.style.transform = 'rotate(-15deg)'
+
+const line2 = document.querySelector('li2')
+li2.style.transform = 'rotate(15deg)'
+
+const line3 = document.querySelector('li3')
+li3.style.transform = 'rotate(30deg)'
+
+const line4 = document.querySelector('li4')
+li4.style.transform = 'rotate(-30deg)'
+
+
+
+
+
